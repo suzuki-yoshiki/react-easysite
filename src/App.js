9ea845e556aaa3feafee68e1ee6ab0b43d546d86
suzukiyoshiki <yoshisu04183@gmail.com>
2022-05-20 21:27:32 +0900

    コンポーネントのライフサイクル

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { List } from "./List";
 import { Form } from "./Form";
-import { LANGUAGES } from "./const/languages";
+import { getLanguages } from "./const/languages";
 
 
 // JSXでのフォーム利用（onChange）
@@ -15,7 +15,17 @@ import { LANGUAGES } from "./const/languages";
 
 function App() {
   const [tab, setTab] = useState("list");
-  const [langs, setLangs] = useState(LANGUAGES);
+  const [langs, setLangs] = useState([]);
+
+  useEffect(() => {
+    console.log("App.js:useEffect");
+    fetchLanguages();
+  }, [])
+
+  const fetchLanguages = async () => {
+    const languages = await getLanguages();
+    setLangs(languages)
+  }
 
   const addLang = (lang) => {
     console.log(lang);
--- src/List.js
+++ src/List.js
@@ -1,4 +1,13 @@
+import { useEffect } from "react"
+
 export const List = ({langs}) => {
+  useEffect(() => {
+    console.log("List.js:useEffect");
+
+    return () => {
+      console.log("List.js:useEffect:unmount");
+    }
+  })
   return (
     <div>
       {
