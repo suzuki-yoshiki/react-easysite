e3c3dcc379f22aecffe0951ab7177ce5895cb64b
suzukiyoshiki <yoshisu04183@gmail.com>
2022-05-22 09:23:02 +0900

    refによるエレメントの取得

--- src/Form.js
+++ src/Form.js
@@ -3,6 +3,7 @@ import { Button } from "./components/button";
 import styled from "styled-components";
 import { TabBodyContainer } from "./components/tab-body-container";
 import { FormModal } from "./FormModal";
+import { Hint } from "./Hint";
 
 
 const Label = styled.label`
@@ -39,6 +40,7 @@ export const Form = ( { onAddLang } ) => {
         <div>
           <Label>言語</Label>
           <Input type="text" value={text} onChange={(e) => setText(e.target.value)} autoFocus/>
+          <Hint />
         </div>
         <div>
           <ButtonContainer>
